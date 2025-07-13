5b55570a22cb4c5fe503d5569cc41fa79bdd8adb
IslamImtiaz <2022me521@student.uet.edu.pk>
2025-07-14 02:15:46 +0500

    TEMPORARY: Enable DEBUG to find production error

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-development-fall
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
-
+# DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
